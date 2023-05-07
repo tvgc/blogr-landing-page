424fc00bfcff610691bf1600d61566cabea59a2c
Thales Victor <thales.v.gc@hotmail.com>
2023-05-07 15:33:29 -0300

    studying

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+const toggle = document.querySelector('.js-toggle');
+const header = document.querySelector('.js-header');
+
+toggle.addEventListener('click', e => {
+    header.classList.toggle('nav-is-open')
+})
+
+
+
+// Seleciona todos os elementos com a classe "accordion-item" e armazena em uma variável
+const itemsFromAccordion = document.querySelectorAll('.js-item-box');
+const items = document.querySelectorAll('.js-item')
+
+// Itera sobre todos os itens selecionados
+itemsFromAccordion.forEach(item => {
+  // Adiciona um evento de clique em cada item
+  item.addEventListener('click', event => {
+    // Verifica se o item clicado já possui a classe "is-open"
+    const isOpen = event.currentTarget.classList.contains('selected');
+
+    // Se já tiver a classe "is-open", remove-a
+    if (isOpen) {
+        document.querySelectorAll('.js-item').forEach(item=>{
+            event.currentTarget.classList.remove('is-open');
+        })
+      
+    } else {
+      // Se não tiver a classe "is-open", adiciona-a e remove de todos os outros elementos
+      itemsFromAccordion.forEach(otherItem => {
+        otherItem.classList.remove('is-open');
+      });
+      event.currentTarget.classList.add('is-open');
+    }
+  });
+});
+
+items.forEach(item=>{
+    item.addEventListener('click', e => {
+        item.parentElement.classList.toggle('is-open')
+    })
+})
